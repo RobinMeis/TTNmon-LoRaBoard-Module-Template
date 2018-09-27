ae1ab3f40dc65db7288c65968dccf479cca8aed5
Robin Meis <robin@meis.space>
2018-09-27 17:41:37 +0200

    Basic files

---
+++ user.cpp
@@ -0,0 +1,21 @@
+#include "user.h"
+
+void user_init(void) { //Hey user, do you want to do some init? Called after VBat check, but before os_init()
+
+}
+
+void user_loop(void) { //Hey user, do you want to do something? Has to be non-blocking and will be called regularly!
+
+}
+
+bool user_uplink(uint8_t *payload, int *length, int *FPort) { //Hey user, do you want to send anything? Otherwise I will only send VBat
+
+}
+
+void user_downlink(int FPort, int dataBeg, int length, uint8_t *payload) { //Hey user, we received downlink. Do you want to do anything?
+
+}
+
+void user_sleep(int minutes) { //Do your own stuff to put controller into sleep if you want.
+  //If not, just leave it empty
+}
